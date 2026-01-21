6871f7f3ebbf70e93a202a9d426a98d721d3a6ea
Bukola <jbukola95@gmail.com>
2026-01-21 18:53:42 +0200

    first commit

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Notifications\LoginNeedsVerification;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+     public function submit(Request $request)
+    {
+        // validate login details
+
+         $request->validate([
+            'phone' => 'required|numeric|min:10',
+        ]);
+
+        // find or create a new user deatils
+
+        $user = User::firstOrCreate([
+            'phone' => $request->phone
+        ]);
+
+        if (!$user) {
+            return response()->json(['message' => 'Could not proceess user with that Phone number.'], 401);
+        }
+
+        // send use a one time code
+
+        //$user->notify(new LoginNeedsVerification());
+
+        //return back a response
+
+        return response()->json([
+            'message' => 'Text Message Notification Sent !!!'
+        ]);
+    }
+
+    public function verify(Request $request)
+    {
+        // validate the incoming request
+
+         $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,999999',
+        ]);
+
+        // find user
+
+        $user = User::where('phone', $request->phone)->where('login_code', $request->login_code)->first();
+
+        // is the code provided the same one saved
+
+        // if so return back on auth token
+
+        if ($user) {
+
+            $user->update([
+                'login_code' => null
+            ]);
+
+            return $user->createToken($request->login_code)->plaintextToken;
+        }
+
+        // if not return back a error message.
+
+        return response()->json(['message' => 'Invalid Verification Code']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\DriverController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/login', [LoginController::class, 'submit']);
+
+ Route::post('/login/verify', [LoginController::class, 'verify']);
+
+ Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/driver', [DriverController::class], 'show');
+    Route::post('/driver', [DriverController::class], 'update');
+
+ Route::post('/trip', ['TripController::class'], 'store');
+ Route::get('/trip/{trip}', ['TripController::class'], 'show');
+
+    Route::get('/user', function(Request $request) {
+        return $request->user();
+    });
+ });
+
