674174052e3a522510993d59631be038664604e7
Bukola <jbukola95@gmail.com>
2026-01-22 11:55:57 +0200

    start a trip to a destination

--- app/Events/TripStarted.php
+++ app/Events/TripStarted.php
@@ -14,12 +14,17 @@ class TripStarted
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $trip;
+
+    private $user;
+
     /**
      * Create a new event instance.
      */
     public function __construct()
     {
-        //
+        $this->trip = $trip;
+        $this->user = $user;
     }
 
     /**
@@ -30,7 +35,7 @@ class TripStarted
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            new Channel('passanger_'. $this->user->id),
         ];
     }
 }
