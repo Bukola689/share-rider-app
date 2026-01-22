8b031f321e55616cbbdfae49f8d764733c312999
Bukola <jbukola95@gmail.com>
2026-01-22 10:46:44 +0200

    Add function into the trip controller and update the model

--- app/Http/Controllers/Api/Auth/TripController.php
+++ app/Http/Controllers/Api/Auth/TripController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\Trip;
 use App\Models\User;
+use App\Models\Driver;
+use App\Notifications\TripAccepted;
+use App\Notifications\TripStarted;
+use App\Notifications\TripEnded;
+use App\Notifications\DriverLocationUpdated;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -23,5 +28,89 @@ class TripController extends Controller
             'destination',
             'destination_name'
         ]));
+    }
+
+    public function show(Request $request, Trip $trip)
+    {
+        // the trip is associated with the authorizeduser or the driver
+        if ($trip->user_id !== $request->user()->id) {
+            return $trip;
+        }
+
+        if($trip->driver && $request->user->driver) {
+            if($trip->driver->id === $request->user()->driver->id) {
+                //return response()->json(['message' => 'Unauthorized'], 403);
+                return $trip;
+            }
+        }
+
+        return response()->json(['message' => 'Cannot Find This Trip'], 404);
+    }
+
+    public function accept(Request $request, Trip $trip)
+    {
+       // Driver accept a Trip
+
+       $request->validate([
+        'driver_location' => 'required'
+       ]);
+
+       //update Trip with driver_id and status
+
+         $trip->update([
+          'driver_id' => $request->user()->id,
+          'driver_location' => $request->driver_location,
+         ]);
+
+         $trip->load('driver.user');
+
+         return $trip;
+    }
+
+    public function start(Request $request, Trip $trip)
+    {
+        //update or start a trip
+
+        $trip->update([
+            'is_started' => true
+        ]);
+
+        $trio->load('driver.user');
+
+        return $trip;
+    }
+
+    public function end(Request $request, Trip $trip)
+    {
+        // final trip destination
+
+         $trip->update([
+            'is_complete' => true
+        ]);
+
+        $trio->load('driver.user');
+
+        return $trip;
+    }
+
+     public function location(Request $request, Trip $trip)
+    {
+        //validate driver location
+
+        $request->validate([
+            'driver_location' => 'required'
+        ]);
+
+        // update the driver location
+
+        $trip->update([
+            'driver_location' => $request->driver_location
+        ]);
+
+        //load relation between user driver
+
+        $trio->load('driver.user');
+
+        return $trip;
     }
 }
